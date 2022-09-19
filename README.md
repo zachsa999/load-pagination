d02a2d5bd80dbe62a6a9941b3cd6e1b7fc379182
zachsa999 <56491639+zachsa999@users.noreply.github.com>
2022-09-19 17:42:11 -0500

    fixed brackets bug, still hate microsoft

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ create a customers.txt file at the root of this directory with format
 ```
 customername|misspelling1|cutstotmer|thereneeds|tobesix|columnsTotal
 ```
-the only important bit is that there is six columns. fill the unneeded columns with junk like `alksdaopisdfo`. I haven't tested it with special charachters, stick to alphanumeric only.
+the only important bit is that there is six columns. fill the unneeded columns with junk like `alksdaopisdfo`. I haven't tested it with special characters, stick to alphanumeric only.
--- sortfiles.ps1
+++ sortfiles.ps1
@@ -1,7 +1,6 @@
 $params = $args
 $rootPath = 'H:\My Drive\(1) ScaleTickets2022\' 
 $weekFolderName = 'week_' + (get-date -UFormat %V) + '_' + (Get-Date -Format "yyyy") + '\'
-$customers = Import-Csv -Path .\customers.txt -Header "filename", "1", "2", "3", "4", "5" -Delimiter "|"
 
 
 function OutputPS {
@@ -12,13 +11,28 @@ function OutputPS {
         Write-Host ("{0}" -f $Text)
     }
 } 
-
+    
 function MoveFiles {
+    Set-Location -Path $rootPath
+    $customers = Import-Csv -Path .\customers.txt -Header "filename", "1", "2", "3", "4", "5" -Delimiter "|"
     OutputPS "Entering Movefiles()"
     OutputPS ('Moving files into corresponding folder in ' + $rootPath + $weekFolderName)
 
+    # create week file after testing if exists
+    if (!(Test-Path -Path ($rootPath + $weekFolderName))) {
+        New-Item -Path ($rootPath + $weekFolderName) -ItemType Directory
+        OutputPS ("folder does not exist Creating folder " + $weekFolderName)
+
+    }
+
+    # Rename all files to lowercase for simple
+    Get-ChildItem -Path ($rootPath) -File | ForEach-Object {
+        Rename-Item $_ -NewName $_.ToString().ToLower()
+
+    }
 
     Get-ChildItem -Path ($rootPath) -File | ForEach-Object {
+
         OutputPS ("Processing " + $_ + " File")
         foreach ($customer in $customers) {
             OutputPS (".. ..")
@@ -26,38 +40,37 @@ function MoveFiles {
             OutputPS ("`X Processing " + $customer.filename + " Folder")
             
             # Test not null
-            $file = $_.ToString() + "*"
-            $fileLower = $_.ToString().ToLower()
-            OutputPS ('O File = ' + $file)
-            if ($fileLower.Contains($customer.filename)`
+            $file = $_.ToString()
+
+            # If a customer matches the email name
+            if ($.Contains($customer.filename)`
                     -or $file.Contains($customer.1)`
                     -or $file.Contains($customer.2)`
                     -or $file.Contains($customer.3)`
                     -or $file.Contains($customer.4)`
                     -or $file.Contains($customer.5)) {
                 
+                # Creates File by 
                 # tests for existing folder
                 if (!(Test-Path -Path ($rootPath + $weekFolderName + $customer.filename))) {
                     New-Item -Path ($rootPath + $weekFolderName + $customer.filename) -ItemType Directory
                     OutputPS ("folder does not exist Creating folder " + $customer.filename)
-                    # }
-
-                    # move $_ into $customer file
-                    OutputPS ("Filepath = " + $rootPath + $_)
-
-                    Move-Item $_ -Destination ($rootPath + $weekFolderName + $customer.filename) 
-                    OutputPS ("^" + $_ + " Moved into " + $customer.filename)
                 }
+
+                # move $_ into $customer file
+                OutputPS ("Filepath = " + $rootPath + $_)
+
+                Move-Item $_ -Destination ($rootPath + $weekFolderName + $customer.filename) 
+                OutputPS ("^" + $_ + " Moved into " + $customer.filename)
             }
-            OutputPS ($rootPath + $_)
-            OutputPS ($rootPath + $weekFolderName).GetType()
-            Move-Item $_ -Destination ($rootPath + $weekFolderName)
         }
-
-        OutputPS "Exiting Movefiles()"
-
+        OutputPS ($rootPath + $_)
+        OutputPS ($rootPath + $weekFolderName).GetType()
+        Move-Item $_ -Destination ($rootPath + $weekFolderName)
     }
+    OutputPS "Exiting Movefiles()"
 }
+
 OutputPS -Text "Begin File"
 
 # CreateLoadFolders
